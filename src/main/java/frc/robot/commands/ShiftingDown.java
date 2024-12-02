8278677c274528f0b3c873da8003d38760acfcd8
broiisapro <moksh.siruvani@gmail.com>
2024-12-02 08:51:11 -0500

    shifting down command for speed modifier

--- src/main/java/frc/robot/commands/ShiftingDown.java
+++ src/main/java/frc/robot/commands/ShiftingDown.java
@@ -1,3 +1,23 @@
-public class ShiftingDown {
-    
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shifting;
+
+public class ShiftingDown extends Command {
+    private final Shifting shifting;
+
+    public ShiftingDown(Shifting shifting) {
+        this.shifting = shifting;
+        addRequirements(shifting);
+    }
+
+    @Override
+    public void initialize() {
+        shifting.decrement();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true; // This command finishes immediately
+    }
 }
