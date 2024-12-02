4aa0e4dfa1d3693b007966a12d345046555a9431
broiisapro <moksh.siruvani@gmail.com>
2024-12-02 11:26:59 -0500

    shifting up command made

--- src/main/java/frc/robot/commands/ShiftingUp.java
+++ src/main/java/frc/robot/commands/ShiftingUp.java
@@ -1,3 +1,23 @@
-public class ShiftingUp {
-    
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shifting;
+
+public class ShiftingUp extends Command {
+    private final Shifting shifting;
+
+    public ShiftingUp(Shifting shifting) {
+        this.shifting = shifting;
+        addRequirements(shifting);
+    }
+
+    @Override
+    public void initialize() {
+        shifting.increment();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true; // This command finishes immediately
+    }
 }
