358cf2311c93d2c01092d057df6fb0d8f69092a6
broiisapro <moksh.siruvani@gmail.com>
2024-12-02 05:47:16 -0500

    made the Shifting sybsystem

--- src/main/java/frc/robot/subsystems/Shifting.java
+++ src/main/java/frc/robot/subsystems/Shifting.java
@@ -1,3 +1,70 @@
-public class Shifting {
-    
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shifting extends SubsystemBase {
+    public static double counter;
+    public static double gear;
+    public static double turnSpeed;
+
+    public Shifting() {
+        counter = 0;
+    }
+
+    public void increment() {
+        counter++;
+        if (counter == 0)
+        {
+            gear = 0;
+        }
+        else if (counter == 1)
+        {
+            gear = 0.6;
+        }
+        else if (counter == 2)
+        {
+            gear = 0.8;
+        }
+        else if (counter == 3)
+        {
+            gear = 1;
+        }
+        else if (counter > 3)
+        {
+            counter = 3;
+            gear = 1.0;
+        }
+        System.out.println("gear: " + counter);
+
+    }
+
+    public void decrement() {
+        counter--;
+        if (counter < 0)
+        {
+            counter = -1;
+            gear = -.7;
+        }
+        else if (counter == 0)
+        {
+            gear = 0;
+        }
+        else if (counter == 1)
+        {
+            gear = 0.6;
+        }
+        else if (counter == 2)
+        {
+            gear = 0.8;
+        }
+        else if (counter == 3)
+        {
+            gear = 1;
+        }
+        System.out.println("gear: " + counter);
+    }
+
+    public double getCounter() {
+        return gear;
+    }
 }
